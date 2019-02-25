2ea3c9cf982b38107f3c7fd3735464ba2d8de273
Nico Sommi <nicosommi@gmail.com>
2019-02-24 22:37:41 -0300

    Updates

---
+++ precache-manifest.dbd23bc3f8919261994acceefd81633b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e876625ef57ceb4c4d82",
+    "url": "/static/js/main.e876625e.chunk.js"
+  },
+  {
+    "revision": "6bf18d1c121452a75a31",
+    "url": "/static/js/2.6bf18d1c.chunk.js"
+  },
+  {
+    "revision": "e876625ef57ceb4c4d82",
+    "url": "/static/css/main.ba845183.chunk.css"
+  },
+  {
+    "revision": "ed792d900150628fe8cfd4ecf47437de",
+    "url": "/index.html"
+  }
+];
