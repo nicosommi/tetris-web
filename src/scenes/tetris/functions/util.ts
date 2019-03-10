852dd8332baf59c2f943e77806edefe1d18b93e7
Nico Sommi <nicosommi@gmail.com>
2019-03-10 12:50:52 -0300

    Basic adjust of the width for small screens

--- src/scenes/tetris/functions/util.ts
+++ src/scenes/tetris/functions/util.ts
@@ -1,7 +1,12 @@
 import C from "chance"
 import { Dimensions } from "react-native"
 import { isWeb } from "../../../utils/debug"
-import { BOARD_LENGTH, BOX_WIDTH, LINE_LENGTH } from "./settings"
+import {
+  BOARD_LENGTH,
+  BOX_WIDTH,
+  LINE_LENGTH,
+  PREVIEW_LINE_LENGTH
+} from "./settings"
 
 export const chance = () => new C()
 
@@ -14,7 +19,9 @@ export const getViewportWidth = () => Dimensions.get("window").width
 export const getViewportHeight = () => Dimensions.get("window").height
 
 function getPotentialBoxMaxWidth() {
-  return Math.floor(getViewportWidth() / LINE_LENGTH)
+  return Math.floor(
+    (getViewportWidth() - 100) / (LINE_LENGTH + PREVIEW_LINE_LENGTH)
+  )
 }
 function getPotentialBoxMaxHeight(joystickCollapsed: boolean) {
   return Math.floor(
