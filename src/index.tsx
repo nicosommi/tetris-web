fbec64d612650a71c5e44d36a517c57f4864b4de
Nico Sommi <nicosommi@gmail.com>
2019-03-17 17:15:57 -0300

    Rollback wrong lock function call

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,6 @@ import App from "./App"
 import "./index.css"
 import * as serviceWorker from "./serviceWorker"
 
-async function lock() {
-  if (screen && screen.orientation && screen.orientation.lock) {
-    try {
-      await screen.orientation.lock("portrait")
-    } catch (e) {
-      // avoid no empty block, intentional swallow
-    }
-  }
-}
-lock()
-
 ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
